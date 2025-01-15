e54a8d7c018b780134f03dedb53caf6a3738b60e
maxim <bozzhik@ya.ru>
2025-01-15 18:52:36 +0300

    upgrade `Button` UI component

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -4,16 +4,27 @@ import Link from 'next/link'
 import {typoClasses} from '~/UI/Typography'
 
 type Props = {
-  to: string
+  to?: string
   target?: '_blank' | '_self'
   text: string
   className?: string
+  onClick?: () => void
 }
 
-export default function Button({to, target = '_self', text, className}: Props) {
+export default function Button({to, target = '_blank', text, className, onClick}: Props) {
+  const buttonStyles = 'block size-fit px-14 xl:px-12 sm:px-6 py-3 sm:py-3.5 xl:py-2.5 lowercase text-center text-white bg-black rounded-lg hover:bg-black/85 duration-300'
+
+  if (to) {
+    return (
+      <Link href={to} className={cn(buttonStyles, typoClasses.p, className)} target={target}>
+        {text}
+      </Link>
+    )
+  }
+
   return (
-    <Link href={to} className={cn('block size-fit px-14 xl:px-12 sm:px-6 py-3 sm:py-3.5 xl:py-2.5 text-center text-white bg-black rounded-lg hover:bg-black/85 duration-300', typoClasses.p, className)} target={target}>
+    <button className={cn(buttonStyles, typoClasses.p, className)} onClick={onClick}>
       {text}
-    </Link>
+    </button>
   )
 }
