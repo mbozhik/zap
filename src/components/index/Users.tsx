d3d2675fed6d191a87bb4d1ce5441d88ce1d70bb
maxim <bozzhik@ya.ru>
2025-01-15 20:41:37 +0300

    fill `sanity` data into `Users` division

--- src/components/index/Users.tsx
+++ src/components/index/Users.tsx
@@ -1,11 +1,14 @@
+import {client, USERS_QUERY, type UsersData} from '@/lib/sanity'
 import {BLOCK_BOX} from '@/lib/constants'
 
 import UsersModule from '~/index/UsersModule'
 
-export default function Users() {
+export default async function Users() {
+  const usersData: UsersData = await client.fetch(USERS_QUERY)
+
   return (
     <section data-section="users-index" className={BLOCK_BOX}>
-      <UsersModule />
+      <UsersModule data={usersData} />
     </section>
   )
 }
--- src/components/index/UsersModule.tsx
+++ src/components/index/UsersModule.tsx
@@ -1,25 +1,54 @@
 'use client'
 
+import {USER_VIEWS, type UserType} from 'sanity/schemaTypes/users'
+import {urlForImage, type UsersData, type GridItem} from '@/lib/sanity'
+
 import {cn} from '@/lib/utils'
 import {useState} from 'react'
 
 import Button from '~/UI/Button'
+import Image from 'next/image'
 
-const views = ['Отправителям', 'Путешественникам'] as const
-type Users = (typeof views)[number]
+export default function UsersModule({data}: {data: UsersData[]}) {
+  const [activeView, setActiveView] = useState<UserType>('Отправителям')
 
-export default function UsersModule() {
-  const [activeView, setActiveView] = useState<Users>('Отправителям')
+  const activeData = data.find((item) => item.type === activeView)
 
   return (
     <section data-section="module-users-index" className="space-y-4">
-      <div className="flex gap-2">
-        {views.map((view) => (
+      <div data-section="switch-module-users" className="flex gap-2">
+        {USER_VIEWS.map((view) => (
           <Button key={view} className={cn('hover:bg-black', activeView === view ? '' : 'bg-black/50')} onClick={() => setActiveView(view)} text={view} />
         ))}
       </div>
 
-      <p>Контент для {activeView === 'Отправителям' ? 'отправителей' : 'путешественников'} здесь.</p>
+      <div data-section="grid-module-users">
+        <div className="grid grid-cols-3 gap-4">
+          {activeData?.grid.map((item, idx) => (
+            <GridItem item={item} key={idx} /> // Dynamic component
+          ))}
+        </div>
+      </div>
     </section>
   )
+}
+
+function GridItem({item}: {item: GridItem}) {
+  if (item.type === 'Контент') {
+    return (
+      <div className={`p-4 bg-${item.background.toLowerCase()}-500`}>
+        {item.icon && <mark>{item.icon}</mark>}
+        {item.heading && <h4 className="font-bold">{item.heading}</h4>}
+        {item.caption && <p>{item.caption}</p>}
+      </div>
+    )
+  } else if (item.type === 'Изображение') {
+    return (
+      <div className="relative w-full h-64">
+        <Image className="object-cover" src={urlForImage(item.image)} alt={item.heading || 'Image'} layout="fill" />
+      </div>
+    )
+  } else {
+    return null
+  }
 }
