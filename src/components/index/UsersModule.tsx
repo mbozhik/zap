27adfa0ad5b2ca785a6e6b8f5e78b2cab51fa51b
maxim <bozzhik@ya.ru>
2025-01-16 19:44:52 +0300

    init `DynamicIcon` UI component
    https://github.com/lucide-icons/lucide/issues/1576#issuecomment-2335019821

--- src/components/index/UsersModule.tsx
+++ src/components/index/UsersModule.tsx
@@ -2,15 +2,16 @@
 
 import {USER_VIEWS, type UserType} from '@/lib/types'
 import {urlForImage, type UsersData, type GridItem} from '@/lib/sanity'
+import {CARD_ROUNDED} from '~/index/Advantages'
 
 import {useState} from 'react'
 import {twMerge} from 'tailwind-merge'
-import {DynamicIcon} from 'lucide-react/dynamic'
 import {cn} from '@/lib/utils'
 
 import Image from 'next/image'
 import Button from '~/UI/Button'
 import {H4, P} from '~/UI/Typography'
+import DynamicIcon from '~/UI/DynamicIcon'
 
 export default function UsersModule({data}: {data: UsersData[]}) {
   const [activeView, setActiveView] = useState<UserType>('Отправителям')
@@ -18,14 +19,14 @@ export default function UsersModule({data}: {data: UsersData[]}) {
   const activeData = data.find((item) => item.type === activeView)
 
   return (
-    <section data-section="module-users-index" className="space-y-4">
-      <div data-section="switch-module-users" className="flex sm:flex-col gap-2">
+    <section data-section="module-users" className="space-y-4">
+      <div data-section="switch-module" className="flex sm:flex-col gap-2">
         {USER_VIEWS.map((view) => (
           <Button key={view} className={cn('hover:bg-black sm:w-full', activeView === view ? '' : 'bg-black/20 text-black hover:text-white')} onClick={() => setActiveView(view)} text={view} />
         ))}
       </div>
 
-      <div data-section="grid-module-users">
+      <div data-section="grid-module">
         <div className={twMerge('grid grid-cols-12 auto-rows-fr gap-4 xl:gap-3 min-h-[65vh] sm:min-h-full', 'sm:flex sm:flex-col')}>
           {activeData?.grid.map((item, index) => (
             <GridItem item={item} idx={index} key={index} /> // Dynamic component
@@ -37,7 +38,6 @@ export default function UsersModule({data}: {data: UsersData[]}) {
 }
 
 function GridItem({item, idx}: {item: GridItem; idx: number}) {
-  console.log('🚀 ~ GridItem ~ item:', item.icon)
   function getGridClass(idx: number) {
     const gridSpanClasses = [
       'col-span-4', // Top-left
@@ -63,7 +63,7 @@ function GridItem({item, idx}: {item: GridItem; idx: number}) {
     if (background === 'Серый') return 'text-black/10'
   }
 
-  const cellStyles = twMerge('relative overflow-hidden', 'rounded-[32px] xl:rounded-3xl sm:rounded-2xl', getBackgroundColor(item.background), getGridClass(idx))
+  const cellStyles = twMerge('relative overflow-hidden', CARD_ROUNDED, getBackgroundColor(item.background), getGridClass(idx))
 
   if (item.type === 'Контент') {
     return (
@@ -71,7 +71,7 @@ function GridItem({item, idx}: {item: GridItem; idx: number}) {
         {item.heading && <H4 className="font-semibold !leading-[1.3]">{item.heading}</H4>}
         {item.caption && <P className="!leading-[1.4]">{item.caption}</P>}
 
-        <DynamicIcon className={cn('absolute top-2 -right-10 size-48', getIconColor(item.background))} strokeWidth={1} name={item.icon} />
+        <DynamicIcon className={cn('absolute top-2 -right-10 size-48', getIconColor(item.background))} name={item.icon} />
       </div>
     )
   } else if (item.type === 'Изображение') {
