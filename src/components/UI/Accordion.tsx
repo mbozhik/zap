75ec49cf4ae0fe22545df0bd717a19f563c2dab8
maxim <bozzhik@ya.ru>
2025-01-16 23:55:34 +0300

    init `Accordion` UI component using `shadcn/ui`
    https://ui.shadcn.com/docs/components/accordion

---
+++ src/components/UI/Accordion.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import * as React from 'react'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import {ChevronDown} from 'lucide-react'
+
+import {cn} from '@/lib/utils'
+
+const Accordion = AccordionPrimitive.Root
+
+const AccordionItem = React.forwardRef<React.ElementRef<typeof AccordionPrimitive.Item>, React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>>(({className, ...props}, ref) => <AccordionPrimitive.Item ref={ref} className={className} {...props} />)
+AccordionItem.displayName = 'AccordionItem'
+
+const AccordionTrigger = React.forwardRef<React.ElementRef<typeof AccordionPrimitive.Trigger>, React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>>(({className, children, ...props}, ref) => (
+  <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Trigger ref={ref} className={cn('flex flex-1 sm:gap-2 items-center justify-between py-4 transition-all hover:bg-gray sm:hover:bg-transparent text-left [&[data-state=open]>svg]:rotate-180 [&[data-state=open]]:bg-gray duration-300', className)} {...props}>
+      {children}
+      <ChevronDown className="!mt-1 size-10 sm:size-8 shrink-0 text-muted-foreground transition-transform duration-300" />
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+))
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+
+const AccordionContent = React.forwardRef<React.ElementRef<typeof AccordionPrimitive.Content>, React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>>(({className, children, ...props}, ref) => (
+  <AccordionPrimitive.Content ref={ref} className="overflow-hidden data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down" {...props}>
+    <div className={className}>{children}</div>
+  </AccordionPrimitive.Content>
+))
+AccordionContent.displayName = AccordionPrimitive.Content.displayName
+
+export {Accordion, AccordionItem, AccordionTrigger, AccordionContent}
--- src/components/index/Questions.tsx
+++ src/components/index/Questions.tsx
@@ -1,5 +1,6 @@
 import type {QuestionBlock} from '@/lib/sanity'
 
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from '~/UI/Accordion'
 import {H1, H4, P} from '~/UI/Typography'
 
 export default function Questions({data}: {data: QuestionBlock[]}) {
@@ -9,10 +10,16 @@ export default function Questions({data}: {data: QuestionBlock[]}) {
 
       <div className="space-y-4">
         {data.map((item, idx) => (
-          <div key={idx} className="px-6 py-4 space-y-2 rounded-xl border-2 border-black">
-            <H4>{item.question}</H4>
-            <P className="pr-5">{item.answer}</P>
-          </div>
+          <Accordion key={idx} type="single" collapsible>
+            <AccordionItem value={`question-${idx}`}>
+              <AccordionTrigger>
+                <H4>{item.question}</H4>
+              </AccordionTrigger>
+              <AccordionContent className="pb-4 pr-10 pt-0">
+                <P>{item.answer}</P>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         ))}
       </div>
     </section>
