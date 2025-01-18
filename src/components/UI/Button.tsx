def42dd6242947ec1717f9535cd6ddc25f4b4875
maxim <bozzhik@ya.ru>
2025-01-18 14:33:14 +0300

    add `magnetic` effect for `Button` UI component
    https://syntaxui.com/components/button/magnetic-button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import {typoClasses} from '~/UI/Typography'
+
 import {cn} from '@/lib/utils'
+import {useRef, useState} from 'react'
+import {motion} from 'motion/react'
 
 import Link from 'next/link'
-import {typoClasses} from '~/UI/Typography'
 
 type Props = {
   to?: string
@@ -12,19 +17,42 @@ type Props = {
 }
 
 export default function Button({to, target = '_blank', text, className, onClick}: Props) {
-  const buttonStyles = 'block size-fit px-14 xl:px-12 sm:px-6 py-3 sm:py-3.5 xl:py-2.5 lowercase text-center text-white bg-black rounded-lg hover:bg-opacity-85 duration-300'
+  const ref = useRef<HTMLButtonElement>(null)
+  const [position, setPosition] = useState<{x: number; y: number}>({x: 0, y: 0})
 
-  if (to) {
-    return (
-      <Link href={to} className={cn(buttonStyles, typoClasses.p, className)} target={target}>
-        {text}
-      </Link>
-    )
+  const handleMouse = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (ref.current) {
+      const {clientX, clientY} = e
+      const {height, width, left, top} = ref.current.getBoundingClientRect()
+      const middleX = clientX - (left + width / 2)
+      const middleY = clientY - (top + height / 2)
+
+      const limitedX = Math.min(Math.max(middleX, -15), 15)
+      const limitedY = Math.min(Math.max(middleY, -15), 15)
+
+      setPosition({x: limitedX, y: limitedY})
+    }
   }
 
+  const reset = () => {
+    setPosition({x: 0, y: 0})
+  }
+
+  const {x, y} = position
+
+  const buttonStyles = 'block size-fit px-14 xl:px-12 sm:px-6 py-3 sm:py-3.5 xl:py-2.5 lowercase text-center text-white bg-black rounded-lg'
+
   return (
-    <button className={cn(buttonStyles, typoClasses.p, className)} onClick={onClick}>
-      {text}
-    </button>
+    <motion.button ref={ref} onMouseMove={handleMouse} onMouseLeave={reset} animate={{x, y}} transition={{type: 'spring', stiffness: 80, damping: 25, mass: 0.5}} className="relative">
+      {to ? (
+        <Link href={to} className={cn(buttonStyles, typoClasses.p, className)} target={target}>
+          {text}
+        </Link>
+      ) : (
+        <div className={cn(buttonStyles, typoClasses.p, className)} onClick={onClick}>
+          {text}
+        </div>
+      )}
+    </motion.button>
   )
 }
