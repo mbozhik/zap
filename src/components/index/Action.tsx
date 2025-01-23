ede369687c32fc202fc5518aa827aba9ccadc24f
maxim <bozzhik@ya.ru>
2025-01-23 17:51:47 +0300

    init `ActionImages` index sub-block

--- src/components/index/Action.tsx
+++ src/components/index/Action.tsx
@@ -1,32 +1,11 @@
-import ActionImage from '$/action.png'
-import MobileActionImage from '$/action-mobile.png'
-
-import type {BlockView} from '@/lib/types'
 import {ACTION_LINK} from '@/lib/constants'
 import {cn} from '@/lib/utils'
 
-import Image from 'next/image'
 import {H1} from '~/UI/Typography'
 import Button from '~/UI/Button'
+import ActionImage from '~/index/ActionImage'
 
 export default function Action() {
-  function ActionImageElem({visible}: {visible: BlockView}) {
-    const imageStyles = {
-      desktop: 'sm:hidden',
-      mobile: 'hidden sm:block sm:!mt-3',
-    }
-
-    return (
-      <Image
-        data-section={`${visible}-image-action`}
-        quality={100}
-        className={cn('block w-full h-full object-contain', imageStyles[visible], visible == 'desktop' && 'col-span-6')}
-        src={visible == 'desktop' ? ActionImage : MobileActionImage} // dynamic image
-        alt="Интерфейс ZAP!"
-      />
-    )
-  }
-
   return (
     <section data-section="action-index" className={cn('grid-cols-10', 'grid items-center xl:gap-20', 'sm:flex sm:flex-col')}>
       <div className={cn('col-span-4', 'space-y-6 xl:space-y-5 sm:space-y-6')}>
@@ -34,12 +13,12 @@ export default function Action() {
           Отправляйте документы с&nbsp;ZAP!
         </H1>
 
-        <ActionImageElem visible="mobile" />
+        <ActionImage visible="mobile" />
 
         <Button to={ACTION_LINK} className="sm:w-full" text="оставить заявку" />
       </div>
 
-      <ActionImageElem visible="desktop" />
+      <ActionImage visible="desktop" />
     </section>
   )
 }
---
+++ src/components/index/ActionImage.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import backgroundHeroImage from '$/action/desktop-background.png'
+import charactersHeroImage from '$/action/desktop.png'
+import MobileActionImage from '$/action/mobile.png'
+
+import type {BlockView} from '@/lib/types'
+
+import {cn} from '@/lib/utils'
+import React, {useRef, useState} from 'react'
+import {motion} from 'motion/react'
+import Image from 'next/image'
+
+export default function ActionImage({visible}: {visible: BlockView}) {
+  const ROTATION_RANGE = 30
+  const HALF_ROTATION_RANGE = ROTATION_RANGE / 2
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  const [rotateX, setRotateX] = useState(0)
+  const [rotateY, setRotateY] = useState(0)
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!ref.current) return
+
+    const rect = ref.current.getBoundingClientRect()
+
+    const width = rect.width
+    const height = rect.height
+
+    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE
+    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE
+
+    const rY = mouseX / width - HALF_ROTATION_RANGE
+    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1
+
+    setRotateX(rX)
+    setRotateY(rY)
+  }
+
+  const handleMouseLeave = () => {
+    if (!ref.current) return
+    setRotateX(0)
+    setRotateY(0)
+  }
+
+  const IMAGE_STYLES = {
+    default: 'block w-full h-full object-contain',
+    visibility: {
+      desktop: 'sm:hidden',
+      mobile: 'hidden sm:block sm:!mt-3',
+    },
+  }
+
+  if (visible === 'desktop') {
+    return (
+      <motion.section
+        data-section="image-action"
+        ref={ref}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          transformStyle: 'preserve-3d',
+        }}
+        animate={{
+          rotateX,
+          rotateY,
+        }}
+        className={cn('col-span-6 relative', IMAGE_STYLES.visibility.desktop)}
+      >
+        <Image
+          priority={true}
+          style={{
+            transform: 'translateZ(100px)',
+            transformStyle: 'preserve-3d',
+          }}
+          src={charactersHeroImage}
+          className={cn('absolute', IMAGE_STYLES.default)}
+          alt="Интерфейс ZAP!"
+        />
+        <Image
+          priority={true}
+          style={{
+            transform: 'translateZ(50px)',
+            transformStyle: 'preserve-3d',
+          }}
+          src={backgroundHeroImage}
+          className={cn('relative z-20', IMAGE_STYLES.default)}
+          alt="Интерфейс ZAP!"
+        />
+      </motion.section>
+    )
+  } else {
+    return <Image data-section={`${visible}-image-action`} quality={100} className={cn(IMAGE_STYLES.default, IMAGE_STYLES.visibility.mobile)} src={MobileActionImage} alt="Интерфейс ZAP!" />
+  }
+}
